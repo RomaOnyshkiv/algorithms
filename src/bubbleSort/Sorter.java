ff045598b534cc3dfb7bb3baecf753eb3ebd19fd
RomaOnyshkiv <romaonyshkiv@gmail.com>
2018-12-05 08:37:36 +0100

    updated swap

--- src/bubbleSort/Sorter.java
+++ src/bubbleSort/Sorter.java
@@ -12,9 +12,9 @@ class Sorter {
         for (int i = 1; i < array.length; i++) {
             for (int j = 0; j < array.length - i; j++) {
                 if (array[j] > array[j + 1]) {
-                    int tmp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = tmp;
+                    array[j] = array[j] + array[j + 1];
+                    array[j + 1] = array[j] - array[j + 1];
+                    array[j] = array[j] - array[j + 1];
                 }
             }
         }
